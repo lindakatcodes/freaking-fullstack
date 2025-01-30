2c3b4b0cb5a111bfd4efacd6515e34c52b5649b6
LindaKat <lindakt16@gmail.com>
2025-01-30 15:10:41 -0600

    created submit link form scaffold

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -3,7 +3,7 @@ import { Metadata } from '@redwoodjs/web'
 const FeedPage = () => {
   return (
     <main className="h-full w-full bg-yellow">
-      <Metadata title="Feed" description="Main news feed page" />
+      <Metadata title="Home" description="Main news feed page" />
       <h1>Main feed page</h1>
     </main>
   )
--- web/src/pages/SubmitLinkPage/SubmitLinkPage.tsx
+++ web/src/pages/SubmitLinkPage/SubmitLinkPage.tsx
@@ -1,21 +1,42 @@
-// import { Link, routes } from '@redwoodjs/router'
+import { FieldError, Form, Label, Submit, UrlField } from '@redwoodjs/forms'
 import { Metadata } from '@redwoodjs/web'
 
 const SubmitLinkPage = () => {
-  return (
-    <>
-      <Metadata title="SubmitLink" description="SubmitLink page" />
+  // copied from https://regexr.com/39nr7 then modified so http is always required
+  const urlRegex =
+    /http(s)?:\/\/[(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
 
-      <h1>SubmitLinkPage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/SubmitLinkPage/SubmitLinkPage.tsx</code>
-      </p>
-      {/*
-          My default route is named `submitLink`, link to me with:
-          `<Link to={routes.submitLink()}>SubmitLink</Link>`
-      */}
-    </>
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <main className="h-full w-full bg-yellow">
+      <Metadata
+        title="SubmitLink"
+        description="Submit a link for the main feed"
+      />
+      <h1>Submit a link</h1>
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="link" className="">
+          Link
+        </Label>
+        <UrlField
+          name="link"
+          className=""
+          errorClassName=""
+          validation={{
+            required: true,
+            pattern: {
+              value: urlRegex,
+              message: 'Links should be valid urls, starting with https://',
+            },
+          }}
+        />
+        <FieldError name="link" className="" />
+        <Submit>Submit</Submit>
+      </Form>
+    </main>
   )
 }
 
