7f021a046eca37bbf55d266f1821e9987249d9d8
LindaKat <lindakt16@gmail.com>
2025-02-27 17:55:11 -0600

    fixed submitLinkForm story

--- web/src/components/SubmitLinkForm/SubmitLinkForm.stories.tsx
+++ web/src/components/SubmitLinkForm/SubmitLinkForm.stories.tsx
@@ -1,15 +1,3 @@
-// Pass props to your component by passing an `args` object to your story
-//
-// ```tsx
-// export const Primary: Story = {
-//  args: {
-//    propName: propValue
-//  }
-// }
-// ```
-//
-// See https://storybook.js.org/docs/7/writing-stories/args
-
 import type { Meta, StoryObj } from '@storybook/react'
 
 import SubmitLinkForm from './SubmitLinkForm'
@@ -17,15 +5,45 @@ import SubmitLinkForm from './SubmitLinkForm'
 const meta: Meta<typeof SubmitLinkForm> = {
   component: SubmitLinkForm,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div className="bg-yellow px-7 py-5">
+        <Story />
+      </div>
+    ),
+  ],
 }
 
 export default meta
 
 type Story = StoryObj<typeof SubmitLinkForm>
 
-export const Primary: Story = {
+export const InitialState: Story = {
   args: {
     loading: false,
     error: null,
+  },
+}
+
+export const FormError: Story = {
+  args: {
+    loading: false,
+    error: {
+      message: 'Links should be valid urls, starting with https://',
+      graphQLErrors: [
+        {
+          message: 'Links should be valid urls, starting with https://',
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            properties: {
+              messages: {
+                url: ['Links should be valid urls, starting with https://'],
+              },
+            },
+          },
+        },
+      ],
+      networkError: null,
+    },
   },
 }
