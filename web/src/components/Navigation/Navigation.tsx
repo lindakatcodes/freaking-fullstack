82e7106e2619bdb231cbfe8f9c25e670407d659a
LindaKat <lindakt16@gmail.com>
2025-02-27 17:13:19 -0600

    fixed navigation story

--- web/src/components/Navigation/Navigation.tsx
+++ web/src/components/Navigation/Navigation.tsx
@@ -6,7 +6,7 @@ const Navigation = () => {
   const { isAuthenticated, logOut } = useAuth()
 
   return (
-    <nav className="absolute top-0 z-10 flex h-11 w-full items-center justify-end gap-1 bg-black p-3 drop-shadow-md">
+    <nav className="absolute top-0 z-10 flex h-12 w-full items-center justify-end gap-1 bg-black p-3 drop-shadow-md">
       {isAuthenticated ? (
         <div className="grid w-full grid-flow-col grid-cols-6 items-center">
           <div className="col-span-4 flex">
