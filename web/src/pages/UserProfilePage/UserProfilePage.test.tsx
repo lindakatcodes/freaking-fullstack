2a361851fa3a4e1d97401b8c5c65c3a133ee6031
LindaKat <lindakt16@gmail.com>
2025-04-08 16:44:05 -0500

    fix: mocked user in user profile page test

--- web/src/pages/UserProfilePage/UserProfilePage.test.tsx
+++ web/src/pages/UserProfilePage/UserProfilePage.test.tsx
@@ -4,6 +4,7 @@ import UserProfilePage from './UserProfilePage'
 
 describe('UserProfilePage', () => {
   it('renders successfully', () => {
+    mockCurrentUser({ id: 1, email: 'hi@test.co' })
     expect(() => {
       render(<UserProfilePage />)
     }).not.toThrow()
